294dd73af2812daf9af9df065b4b14c88fbcd0a2
ismail <ismail.x@aol.com>
2020-09-20 05:03:19 +0700

    iniT

---
+++ Cov19/Petition.swift
@@ -0,0 +1,14 @@
+//
+//  Berita.swift
+//  Cov19
+//
+//  Created by Ismail . on 20/09/20.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
---
+++ Cov19/Petitions.swift
@@ -0,0 +1,12 @@
+//
+//  Petitions.swift
+//  Cov19
+//
+//  Created by Ismail . on 20/09/20.
+//
+
+import Foundation
+
+struct Petitions: Codable  {
+    var results: [Petition]
+}
