5230033352b801e92e84d6dd9fe43a84a436146f
ismail <ismail.x@aol.com>
2020-09-20 03:10:49 +0700

    Initial Commit

---
+++ Cov19/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cov19
+//
+//  Created by Ismail . on 20/09/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
