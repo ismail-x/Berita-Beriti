2ab9808c33a1487ed5b77d288a2f560eb10a371e
ismail <ismail.x@aol.com>
2020-09-21 01:32:54 +0700

    done

--- Cov19/ViewController.swift
+++ Cov19/ViewController.swift
@@ -13,13 +13,26 @@ class ViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
-        
+        let urlString: String
+        if navigationController?.tabBarItem.tag == 0 {
+           urlString = "https://www.hackingwithswift.com/samples/petitions-1.json"
+        } else {
+            urlString = "https://www.hackingwithswift.com/samples/petitions-2.json"
+        }
+           
         if let url = URL(string: urlString){
             if let data = try? Data(contentsOf: url) {
                 parse(json: data)
+            return
             }
         }
+        showError()
+    }
+    
+    func showError() {
+        let ac = UIAlertController(title: "Loading error", message: "there was a problem loading the feed: please check your connection", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "OK", style: .default))
+        present(ac, animated: true)
     }
     
     func parse(json: Data){
@@ -42,6 +55,12 @@ class ViewController: UITableViewController {
         cell.textLabel?.text = petition.title
         cell.detailTextLabel?.text = petition.body
         return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let vc = DetailViewController()
+        vc.detailItem = petitions[indexPath.row]
+        navigationController?.pushViewController(vc, animated: true)
     }
 
 }
